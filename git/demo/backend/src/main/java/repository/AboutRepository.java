f2a1274738e6166f6354f586b9540ea52c5f361e
goalkeeper91 <goalkeeper91cast@gmail.com>
2025-07-21 16:09:13 +0200

    Revert "initial commit"
    This reverts commit 366013e721b4e1773ff759ebd7f13c28bb4213a6.

--- git/demo/backend/src/main/java/repository/AboutRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import entity.About;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AboutRepository extends JpaRepository<About, Long> {
-}
