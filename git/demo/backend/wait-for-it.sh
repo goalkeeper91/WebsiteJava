e5b5784e5db45250c2014f506e0721a8e263d8dd
goalkeeper91 <goalkeeper91cast@gmail.com>
2025-08-23 16:37:49 +0200

    refactored docker compose

--- git/demo/backend/wait-for-it.sh
+++ git/demo/backend/wait-for-it.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 # wait-for-it.sh
 
-# Warten, bis der PostgreSQL-Host bereit ist
-# (hier verwenden wir 'postgres' als Host-Namen, wie im docker-compose.yml definiert)
+# Variablen für den Host-Namen und den auszuführenden Befehl
 host="$1"
 shift
 cmd="$@"
 
+# Warte auf das Vorhandensein der Datenbank und der notwendigen Tabelle
+# (Verwendet psql um auf die Verfügbarkeit der 'twitch_commands' Tabelle zu warten)
 until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
   >&2 echo "Postgres ist noch nicht verfügbar - schläft"
   sleep 1
 done
 
->&2 echo "Postgres ist bereit - Anwendung starten"
+# Wenn der Datenbank-Server bereit ist, warte auf die Liquibase-Migration
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "SELECT 1 FROM twitch_commands LIMIT 1;" > /dev/null 2>&1; do
+  >&2 echo "Warte auf die Liquibase-Migration und die Tabelle 'twitch_commands'..."
+  sleep 1
+done
+
+>&2 echo "Datenbank und Schema sind bereit. Anwendung starten."
 exec $cmd
